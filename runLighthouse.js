69ba565acefe9f027d73197a50c207524451cb45
Peter Hedenskog <peter@soulgalore.com>
2022-02-07 18:38:31 +0100

    Catch and log if Chrome cannot start (#97)

--- runLighthouse.js
+++ runLighthouse.js
@@ -2,9 +2,19 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 async function runLighthouse(url, flags, config, chromeFlags, log) {
-  const chrome = await chromeLauncher.launch({ chromeFlags });
-  flags.port = chrome.port;
-  flags.output = 'html';
+  let chrome;
+  try {
+    chrome = await chromeLauncher.launch({ chromeFlags });
+    flags.port = chrome.port;
+    flags.output = 'html';
+  } catch (e) {
+    log.error(
+      'Could not start Chrome with flags: %:2j and error %s',
+      chromeFlags,
+      e
+    );
+    throw e;
+  }
 
   let result = {};
   try {
