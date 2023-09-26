5cd1b461547a674862385d26e824767c11becdb8
Peter Hedenskog <peter@soulgalore.com>
2023-09-26 15:04:42 +0200

    Lighthouse 11.1.0 (#119)
    * Lighthouse 11.1.0
    
    * Chrome runner fix

--- runLighthouse.js
+++ runLighthouse.js
@@ -1,5 +1,5 @@
 import lighthouse from 'lighthouse';
-import chromeLauncher from 'chrome-launcher';
+import * as chromeLauncher from 'chrome-launcher';
 
 export async function runLighthouse(url, flags, config, chromeFlags, log) {
   let chrome;
